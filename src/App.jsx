6cf12fd51ea2782db70243841e5ce3aba7df8e4b
“KL-2300032590” <2300032590@kluniversity.in>
2025-07-29 12:06:14 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <div className="container">
       <h1>Welcome to My React Project - S205</h1>
       <p>CI/CD and Cloud DevOps</p>
-      <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
+      <p>This is a simple homepage deployed on GitHub <span>Actions</span> using Vite.</p>
     </div>
   );
 }
